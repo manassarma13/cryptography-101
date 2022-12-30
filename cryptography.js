ace9c53251c2b6ad1ece12a4bad1f2d1f04e5f10
M4N4$ $4RM4 <manassarma121@gmail.com>
2022-12-30 14:58:24 +0530

    Add files via upload

---
+++ cryptography.js
@@ -0,0 +1,29 @@
+//simple example of how cryptography can be implemented in Node.js using a library like crypto, which is a built-in module in Node.js.
+//Thanks yo
+
+const crypto = require('crypto');
+
+// Generate a random secret key
+const key = crypto.randomBytes(32);
+
+// Initialize the cipher
+const cipher = crypto.createCipheriv('aes-256-gcm', key, Buffer.alloc(16));
+
+// Encrypt the data
+const data = Buffer.from('hello world');
+let ciphertext = cipher.update(data);
+ciphertext = Buffer.concat([ciphertext, cipher.final()]);
+const tag = cipher.getAuthTag();
+
+// Decrypt the data
+const decipher = crypto.createDecipheriv('aes-256-gcm', key, Buffer.alloc(16));
+decipher.setAuthTag(tag);
+let plaintext = decipher.update(ciphertext);
+plaintext = Buffer.concat([plaintext, decipher.final()]);
+
+// Verify that the decryption was successful
+if (data.toString() === plaintext.toString()) {
+    console.log('The message was decrypted successfully');
+} else {
+    console.log('The message was tampered with');
+}
